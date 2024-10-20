4e3eae8fe48fca6c8805d41113419fd6c936793d
Your Name <you@example.com>
2024-10-20 20:38:45 +0530

    Reinstall dependencies

--- src/AsianPaints.jsx
+++ src/AsianPaints.jsx
@@ -7,11 +7,13 @@ export const AsianPaints = () => {
   return (
     <div className='p-0 mx-0'>
         <div className='w-full flex items-center justify-center h-[600px]'>
-            <div className='inset-0 w-full h-full bg-primaryyel/70 blur-xl'>
+            <div className='inset-0 w-full h-full bg-primaryyel/70'>
                 <div className= 'justify-center items-center flex pt-[7%] z-10'>
                     <div className='flex gap-72'>
-                    <div className='w-[500px] h-[400px] bg-gray-100/40'></div>
-                    <div className='pt-0'><img width={500} src={Asian} alt="" /></div>
+                    <div className='w-[500px] h-[400px] bg-gray-100/40 flex items-center justify-center'>
+                        <p className='text-black z-20 font-kavivanar text-[20px] p-8'><span>    </span> We have been proudly dealing with Asian Paints     since 2023 and are an authorized dealer for the   brand. With over 2200+ colors available, we offer a wide range of options to help you create the perfect look for your home. Trust us for quality, variety, and expert advice on all your painting needs!</p>
+                    </div>
+                    <div className='pt-0 '><img width={500} src={Asian} alt="" /></div>
                     </div>
                     
                 </div>
--- src/Ourdeals.jsx
+++ src/Ourdeals.jsx
@@ -9,12 +9,12 @@ export const Ourdeals = () => {
     <div className=''>
         <div className='pt-10 pl-[10%]'><p className='text-[25px] font-extrabold font-itim '><span className='text-[40px]'>D</span>ealing With !</p></div>
         <div className='flex gap-8 pl-[7%] pt-4'>
-            <div className='hover:scale-110 rounded-2xl w-[240px] h-[80px] border-2'><p style={{ textShadow: '12px 8px 9px rgba(0, 0, 0, 0.25)'}} className='text-[25px] font-bold py-5 px-10 items-center'><span className='text-primaryred'>Asian</span><span className='text-primaryyel'> Paints</span></p></div>
-            <div className='hover:scale-110 rounded-2xl w-[240px] px-7 h-[80px] border-2'>
+            <div className='hover:scale-110  hover:shadow-2xl rounded-2xl w-[240px] h-[80px] border-2'><p style={{ textShadow: '12px 8px 9px rgba(0, 0, 0, 0.25)'}} className='text-[25px] font-bold py-5 px-10 items-center'><span className='text-primaryred'>Asian</span><span className='text-primaryyel'> Paints</span></p></div>
+            <div className='hover:scale-110 hover:shadow-2xl rounded-2xl w-[240px] px-7 h-[80px] border-2'>
                 <img src={Surya} width={180} alt="" className='' />
             </div>
-            <div className='hover:scale-110 rounded-2xl w-[240px] px-14 h-[80px] border-2'><img src={Mark} width={170}  alt="" className=''/></div>
-            <div className='hover:scale-110 rounded-2xl w-[240px] px-11  py-3 h-[80px] border-2'>
+            <div className='hover:scale-110 hover:shadow-2xl  rounded-2xl w-[240px] px-14 h-[80px] border-2'><img src={Mark} width={170}  alt="" className=''/></div>
+            <div className='hover:scale-110 hover:shadow-2xl  rounded-2xl w-[240px] px-11  py-3 h-[80px] border-2'>
                 <img src={Ramco} width={150} alt="" className=''/>
             </div>
             <div className='hover:scale-110 rounded-2xl w-[240px] h-[80px] border-2 '></div>
